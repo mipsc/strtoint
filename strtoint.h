39e09b046c9fbdd3af493c26575e6e03e0da4a1f
mipsc <50635548+mipsc@users.noreply.github.com>
2019-06-16 22:04:33 +0200

    Create strtoint.h

---
+++ strtoint.h
@@ -0,0 +1,9 @@
+//  Created by mipsc on 16.06.19.
+
+#ifndef strtoint_h
+#define strtoint_h
+
+// A simple function to convert a string to int.
+int strtoint(const char* str);
+
+#endif /* strtoint_h */
